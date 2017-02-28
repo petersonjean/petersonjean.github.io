0feef07f7842243fdf6fb6e9014a958a14ba310e
Peterson JEAN <pejean@uhelp.net>
2017-02-28 00:23:48 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,36 @@
- 
-  $(".menu-item a").click(function(){ 
-                   $(".active").removeClass("active");   
-                   $(this).closest('li').addClass("active");
-                          });
- 
- 
+//animating the menu icon button on mobile view
+function animationBtnMenu(bar){
+ bar.classList.toggle("animation-btn-menu");
+$(".nav").toggle("show",function(){
+  
+});
+}
+
+$(document).ready(function(){
+  //active the current menu item in nav menu
+  $(".menu-item a").click(function(){
+                    $(".active").removeClass("active");
+                    $(this).closest('li').addClass("active");
+                           });
+  $(".menu-icon").hide();
+
+
+  //changing menu depepnding on screen size
+  if( $(window).width() > 480){
+    $(".nav").addClass('nav-pills');
+  }else{
+    $(".nav").hide();
+  }
+  $(window).on('resize', function() {
+    if( $(window).width() > 480){
+      $(".nav").addClass('nav-pills');
+        $(".nav").show();
+    }else{
+        $(".nav").removeClass('nav-pills');
+        $(".nav").hide();
+    }
+ });
+
+
+
+});
